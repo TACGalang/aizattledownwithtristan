33f6a3f154559a77ef3d469f6af0aa1d0f47a9cb
Tristan Angelo C. Galang <tac.galang@gmail.com>
2021-11-27 19:35:37 +0800

    Added hamburger menu

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import NavBar from "../Components/NavBar";
 import "./Home.css";
 
 function Home() {
+  const [menuClicked, setMenuClicked] = useState(false);
+  const [menuHidden, setMenuHidden] = useState(false);
+  const handleClicked = () => setMenuClicked(!menuClicked);
+
+  const changeMenuIconVisibility = () => {
+    if (window.scrollY >= 100) {
+      setMenuHidden(false);
+    } else {
+      setMenuHidden(true);
+    }
+  };
+
+  window.addEventListener("scroll", changeMenuIconVisibility);
+
   return (
     <div className="home-page">
+      <div
+        className={menuHidden ? "menu-icon-inactive" : "menu-icon-active"}
+        onClick={handleClicked}
+      >
+        <i className={menuClicked ? "fa fa-times" : "fas fa-bars"} />
+      </div>
       <Header />
       <NavBar />
       <div className="storyDIV" />
