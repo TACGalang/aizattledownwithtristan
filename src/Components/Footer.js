8a8a3c4a0cd496d6abf1491a56bef41c8dcb8de1
Tristan Angelo C. Galang <tac.galang@gmail.com>
2021-12-02 07:10:29 +0800

    Add logo in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "./Button";
 import "./Footer.css";
+import weddingLogo from "../Images/wedding_logo.png";
 
 function Footer() {
   return (
     <div className="footer-container">
       <div>
+        <img src={weddingLogo} />
         <h1>We can't wait to see you!</h1>
         <p>Please let us know if you'll be able to make it.</p>
         <Button
